f03791b2b7f0c33c27ac24639bd7bd910a865ae3
Leon K.C. <leonkc10@gmail.com>
2025-11-20 10:32:58 +0545

    Created new java file

---
+++ javanew1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class javanew1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class javanew1
+{
+    public static void main(String[] args){
+        
+        System.out.println("Learning git with BlueJ");
+        
+    }
+}
